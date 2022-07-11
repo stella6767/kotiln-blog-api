0cb87bbabbbd984f296dd128003fd6a2ce45f1d6
kang <gangmingyu@gangmingyuui-MacBookAir.local>
2022-07-11 23:05:47 +0900

    kotiln jdsl orderby query

--- src/main/kotlin/com/example/simpleblog/domain/member/MemberRepository.kt
+++ src/main/kotlin/com/example/simpleblog/domain/member/MemberRepository.kt
@@ -1,6 +1,9 @@
 package com.example.simpleblog.domain.member
 
+import com.linecorp.kotlinjdsl.query.spec.ExpressionOrderSpec
 import com.linecorp.kotlinjdsl.query.spec.OrderSpec
+import com.linecorp.kotlinjdsl.query.spec.expression.ColumnSpec
+import com.linecorp.kotlinjdsl.querydsl.expression.column
 import com.linecorp.kotlinjdsl.querydsl.orderby.OrderByDsl
 import com.linecorp.kotlinjdsl.spring.data.SpringDataQueryFactory
 import com.linecorp.kotlinjdsl.spring.data.listQuery
@@ -24,23 +27,29 @@ class MemberCustomRepositoryImpl(
 
     override fun findAllByPage(pageable: Pageable): Page<Member> {
 
-        
 
         val members = queryFactory.listQuery<Member> {
             select(entity(Member::class))
             from(entity(Member::class))
-            //orderBy()
-
             limit(pageable.pageSize)
             offset(pageable.offset.toInt())
-
+            orderBy(ExpressionOrderSpec(expression = column(Member::id),
+                    ascending = false))
         }
+
+//        queryFactory.listQuery<String> {
+//            select(column(Member::email))
+//            from(entity(Member::class))
+//        }
 
         val countQuery = queryFactory.listQuery<Member> {
             select(entity(Member::class))
             from(entity(Member::class))
         }
 
+
+//        return PageImpl( members.sortedByDescending { it.id}
+//                , pageable, countQuery.size.toLong())
         return PageImpl(members, pageable, countQuery.size.toLong())
     }
 
